6ca3e9ef96e3983b786bd4b25c7f4ddcd1882fbd
Cristina Rodriguez <yosola@gmail.com>
2022-12-01 13:08:23 -0800

    Day one solved

--- day01/day01example.js
+++ day01/day01example.js
@@ -1,8 +1,44 @@
 const fs = require("fs");
 
 const lines = fs
-  .readFileSync("day01example.txt", { encoding: "utf-8" })
+  .readFileSync("inputday1.txt", { encoding: "utf-8" })
   .split("\n")
   .map((x) => parseInt(x));
 
-console.log(lines);
+
+const maxElfData = (array) => {
+  let elf = 0;
+  let maxElf = 0;
+  for (let calories of array) {
+    if (!isNaN(calories)) {
+      elf += calories;
+    } else {
+      if (elf > maxElf) {
+        maxElf = elf;
+      }
+      elf = 0;
+    }
+  }
+  return maxElf;
+
+}
+
+const top3Elf = (array) => {
+  let elf = 0;
+  let tempArray = [];
+  for (let i = 0; i < array.length + 1; i++) {
+    if (!isNaN(array[i])) {
+      elf += array[i];
+    } else {
+      tempArray.push(elf);
+      elf = 0;
+    }
+  }
+
+  let sortTemp = tempArray.sort((a, b) => b - a)
+  let top3 = (sortTemp[0] + sortTemp[1] + sortTemp[2])
+  return top3
+}
+//console.log(lines);
+console.log(maxElfData(lines));
+console.log(top3Elf(lines));
