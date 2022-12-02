e98ca2680724e3e77fb20578bd62b6b1426d434f
Cristina Rodriguez <yosola@gmail.com>
2022-12-02 13:15:22 -0800

    first part day2 correct

--- day02/day02code.js
+++ day02/day02code.js
@@ -1,7 +1,42 @@
 const fs = require("fs");
 
 const lines = fs
-  .readFileSync("day02example.txt", { encoding: "utf-8" })
-  .split("\n")
+    .readFileSync("inputday2.txt", { encoding: "utf-8" })
+    .split("\n")
+    .map((line) => line.split(" "));
 
-console.log(lines);
+//console.log(lines);
+
+let rules = { rock: "scissors", scissors: "paper", paper: "rock" }
+
+let pointsbyShape = { rock: 1, paper: 2, scissors: 3 }
+let pointsbyGame = { lost: 0, draw: 3, win: 6 };
+
+let trans = { A: "rock", X: "rock", B: "paper", Y: "paper", C: "scissors", Z: "scissors" };
+
+function gameTotal(input) {
+    let totalScore = 0;
+    for (let game of input) {
+        let [opponent, me] = game;
+        let roundScore = round(opponent, me);
+        console.log(roundScore, "roundscore for game");
+        totalScore += roundScore;
+    }
+    return totalScore;
+}
+
+function round(opponent, me) {
+    let score = 0;
+    let opponentPlay = trans[opponent];
+    let mePlay = trans[me];
+    // Points I gain just for playing a Shape
+    score = pointsbyShape[mePlay];
+    if (rules[mePlay] === opponentPlay) {
+        score += pointsbyGame.win;
+    } else if (mePlay === opponentPlay) {
+        score += pointsbyGame.draw;
+    }
+    return score;
+}
+
+console.log(gameTotal(lines));
